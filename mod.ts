78f93b4301a58debcac9d8ec486add7abab2d4cc
AureGitHub <auredeveloper@gmail.com>
2024-03-07 13:03:34 +0100

    add mod.ts

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export {run} from "./server.ts"
+export * from "./dep/deps.ts"
+export * from "./utils/status.ts"
+export * from "./utils/sendEmail.ts"
+
+export * from "./aureDB/aureDB.ts"
+export * from "./AUTH/controller.ts"
